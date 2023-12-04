9b5157fafc2e837070b3b33e2379145c3e25d011
apesmetdf <152496563+apesmetdf@users.noreply.github.com>
2023-12-04 21:20:35 +0100

    computepi

--- app.py
+++ app.py
@@ -1,14 +1,50 @@
 from flask import Flask
 import os
 
+def calcPi(limit):  # Generator function
+    """
+    Prints out the digits of PI
+    until it reaches the given limit
+    """
+    q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
+
+    decimal = int(limit)
+    counter = 0
+
+    while counter != decimal + 1:
+            if 4 * q + r - t < n * t:
+                    # yield digit
+                    yield n
+                    # insert period after first digit
+                    if counter == 0:
+                            yield '.'
+                    # end
+                    if decimal == counter:
+                            print('')
+                            break
+                    counter += 1
+                    nr = 10 * (r - n * t)
+                    n = ((10 * (3 * q + r)) // t) - 10 * n
+                    q *= 10
+                    r = nr
+            else:
+                    nr = (2 * q + r) * l
+                    nn = (q * (7 * k) + 2 + (r * l)) // (t * l)
+                    q *= k
+                    t *= l
+                    l += 2
+                    k += 1
+                    n = nn
+                    r = nr
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World, I am Antoine 2!"
+@app.route('/computepi<ndecimal>')
+def hello(ndecimal):
+    return ''.join(str(i) for i in calcPi(ndecimal))
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
-
     app.run(port=port,host='0.0.0.0')
